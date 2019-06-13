2f746c04c185d0402850963b9be3b441ed6b5dc1
harry <harry74323@gmail.com>
2019-06-13 09:49:55 +0800

    Revert "clean up"
    This reverts commit 3f4f0212af24d460bfe5dfc833e3eed25c0b221a.

--- src/index.js
+++ src/index.js
@@ -1,20 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
-import Counter from './components/Counter'
-import counter from './reducers'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-const store = createStore(counter)
-const rootEl = document.getElementById('root')
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
 
-const render = () => ReactDOM.render(
-  <Counter
-    value={store.getState()}
-    onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
-    onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
-  />,
-  rootEl
-)
-
-render()
-store.subscribe(render)
